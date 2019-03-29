4024560304fda5872fd0220cf29ae251f9eae1c9
pigbog <mesteves@gmail.com>
2019-03-28 20:35:26 -0400

    Added ability to increase modifiers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Mash from './components/mash';
+
 import './styles/App.css';
 
 class App extends Component {
@@ -16,12 +17,22 @@ class App extends Component {
     this.setState({ goo: newGoo });
   };
 
-  automash = () => {
+  autoMash = () => {
     setInterval(() => {
       let autoGoo = this.state.goo + this.state.autoGooMod;
       this.setState({ goo: autoGoo });
     }, 1000);
   };
+
+  autoMashIncrease = num => {
+    let newMod = this.state.autoGooMod + num;
+    this.setState({ autoGooMod: newMod });
+  };
+  gooModIncrease = num => {
+    let newMod = this.state.gooMod + num;
+    this.setState({ gooMod: newMod });
+  };
+
   render() {
     return (
       <Mash goo={this.state.goo} mash={this.mash} automash={this.automash} />
